0ec0db8e00603ce5aaece19669f1bae132ad6f6e
Rodolphe Vidal <54648689+Setsin@users.noreply.github.com>
2020-07-31 21:35:46 +0200

    Récupération des datasets

---
+++ get_csv.py
@@ -0,0 +1,15 @@
+from yahoo_fin.stock_info import get_data, tickers_sp500, tickers_nasdaq, tickers_other, get_quote_table
+import pandas as pd
+
+
+BTC = get_data("BTC-USD")
+
+df = pd.DataFrame(BTC, index=None)
+
+df.to_csv(path_or_buf="C:/Users\Administrateur\Documents\GitHub\Projet\Data/BTC-USD.csv")
+
+# Dataset post-bullrun 2017
+
+post_bull = get_data('BTC-USD', start_date = "21-06-2018")
+df2 = pd.DataFrame(post_bull)
+df2.to_csv(path_or_buf="C:/Users\Administrateur\Documents\GitHub\Projet\Data/BTC-USD(PB).csv")
