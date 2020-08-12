5e024803e74383a73ff3b91663b2ee2c5746a412
Rodolphe Vidal <54648689+Setsin@users.noreply.github.com>
2020-08-12 17:56:19 +0200

    Ajout du test de Dickey-Fuller
    La serie n'est pas stationnaire

---
+++ models/model_ARIMA.py
@@ -0,0 +1,13 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from scipy import stats
+import statsmodels.api as sm
+import warnings
+from itertools import product
+from datetime import datetime
+warnings.filterwarnings('ignore')
+plt.style.use('seaborn-poster')
