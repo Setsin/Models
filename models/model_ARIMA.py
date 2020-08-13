756cae1e129dd2eddea45633631ff361901ad5e4
Rodolphe Vidal <54648689+Setsin@users.noreply.github.com>
2020-08-13 12:54:03 +0200

    Ajout model ARIMA

--- models/model_ARIMA.py
+++ models/model_ARIMA.py
@@ -9,5 +9,19 @@ import statsmodels.api as sm
 import warnings
 from itertools import product
 from datetime import datetime
+import data_variables.py 
+
 warnings.filterwarnings('ignore')
 plt.style.use('seaborn-poster')
+
+# Initial approximation of parameters using Autocorrelation and Partial Autocorrelation Plots
+plt.figure(figsize=(15,7))
+ax = plt.subplot(211)
+sm.graphics.tsa.plot_acf(btc_month.adjclose_box_diff2[13:].values.squeeze(), lags=48, ax=ax)
+ax = plt.subplot(212)
+sm.graphics.tsa.plot_pacf(btc_month.adjclose_box_diff2[13:].values.squeeze(), lags=48, ax=ax)
+plt.tight_layout()
+plt.show()
+
+
+
