243e09833982bd2da42d8162c476756713c3ead0
Rodolphe Vidal <54648689+Setsin@users.noreply.github.com>
2020-08-20 13:11:15 +0200

    update

--- get_csv.py
+++ get_csv.py
@@ -1,12 +1,15 @@
 from yahoo_fin.stock_info import get_data, tickers_sp500, tickers_nasdaq, tickers_other, get_quote_table
 import pandas as pd
 
-# Dataset all-data
+# Dataset all-data (+ Rename first column )
 BTC = get_data("BTC-USD")
 df = pd.DataFrame(BTC, index=None)
+df.rename(columns={'Unnamed: 0':'Date'}, inplace=True)
 df.to_csv(path_or_buf="./data/BTC-USD.csv")
 
-# Dataset post-bullrun 2017
+# Dataset post-bullrun 2017 (+ Rename Date column)
 post_bull = get_data('BTC-USD', start_date = "21-06-2018")
 df2 = pd.DataFrame(post_bull)
+df2.rename(columns={'Unnamed: 0':'Date'}, inplace=True)
 df2.to_csv(path_or_buf="./data/BTC-USD(PB).csv")
+print(df2['Date'])
