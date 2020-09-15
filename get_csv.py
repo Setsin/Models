0a580da0c0ddb7f47b73500f9bf1f52fdec50465
Setsin <rodolphevdl@gmail.com>
2020-09-15 15:47:02 +0200

    update

--- get_csv.py
+++ get_csv.py
@@ -7,14 +7,10 @@ df = pd.DataFrame(BTC, index=None)
 df.rename(columns={'Unnamed: 0':'Date'}, inplace=True)
 df.to_csv(path_or_buf="./data/BTC-USD.csv")
 
-<<<<<<< HEAD
 df.to_csv(path_or_buf="./data/BTC-USD.csv")
 
 # Dataset post-bullrun 2017
 
-=======
-# Dataset post-bullrun 2017 (+ Rename Date column)
->>>>>>> a2ec96ae43caa309b703f5fee2b55e3a16098ccd
 post_bull = get_data('BTC-USD', start_date = "21-06-2018")
 df2 = pd.DataFrame(post_bull)
 df2.rename(columns={'Unnamed: 0':'Date'}, inplace=True)
